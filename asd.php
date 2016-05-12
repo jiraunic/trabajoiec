8ade18086187458a0aa3992bc7dd2667c1ad75d8
jiraunic <juanirodrigueza@gmail.com>
2016-05-12 16:23:49 -0500

    asdasd

--- asd.php
+++ asd.php
@@ -12,7 +12,9 @@ $count=0;
 $sql="select Nombre_usuario,pass from usuarios where Nombre_usuario='$user' and pass='$password';";
 echo "vamos bien, antes de entrar en if : ";
 $result=mysqli_query($con,$sql);
+
  printf("se devolvio  %d filas.\n", mysqli_num_rows($result));
+ mysqli_free_result($result);
  echo $user;
  echo $password;
 if ($result=mysqli_query($con,$sql))
