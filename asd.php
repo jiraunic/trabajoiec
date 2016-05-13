33468fa41fae1c18fe4f8d804849c6c6fb545c79
jiraunic <juanirodrigueza@gmail.com>
2016-05-12 20:11:26 -0500

    asdasd
    asdasd

--- asd.php
+++ asd.php
@@ -10,7 +10,7 @@ $user=$_POST['user'];
 $password = $_POST['password'];
 
 $count=0;
-$sql="select Nombre_usuario, pass from usuarios ";
+$sql="select * from usuarios; ";
 echo "vamos bien, antes de entrar en if : ";
 $result=mysqli_query($con, $sql);
 printf("La seleccion devolvio %d filas.\n", mysqli_num_rows($result));
