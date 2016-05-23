b13b916a95200085489626fcd885f258c1df4b86
jiraunic <juanirodrigueza@gmail.com>
2016-05-23 17:14:53 -0500

    asdasd
    asdasd

--- asd.php
+++ asd.php
@@ -4,9 +4,9 @@ $user=$_POST['user'];
 $password = $_POST['password'];
 
 $count=0;
-$sql="SELECT * from usuarios";
+$sql="SELECT * from usuarios where  Nombre_usuario='$user' and pass='$password'";
 if ($result=mysql_query($sql))
-  {
+  {echo "si entramos";
   while ($obj=mysql_fetch_object($result))
     {
       $count=$count+1;
