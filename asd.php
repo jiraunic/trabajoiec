641129b5c05807f6c296096ad4edfd7f985cad8b
jiraunic <juanirodrigueza@gmail.com>
2016-05-23 17:11:29 -0500

    asdasdsa
    dasdas

--- asd.php
+++ asd.php
@@ -1,21 +1,21 @@
 <?php
 include_once "Conexion.php";
-//$user=$_POST['user'];
-//$password = $_POST['password'];
+$user=$_POST['user'];
+$password = $_POST['password'];
 
-//$count=0;
+$count=0;
 $sql="SELECT * from usuarios";
 if ($result=mysql_query($sql))
   {
   while ($obj=mysql_fetch_object($result))
-    { echo "Entramos al while : ";
+    {
       $count=$count+1;
     }
 
 }
 if ($count==1) {
 	echo "Entramos al if correctamente";
-    header("location:inicio.php");  
+   header('Location: Inicio.php');
 }
 else
 {
