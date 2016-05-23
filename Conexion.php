a15dbbe59e809e67ae0a31672fdd10d84bb6af5b
jiraunic <juanirodrigueza@gmail.com>
2016-05-23 16:34:43 -0500

    asdas
    dasd

--- Conexion.php
+++ Conexion.php
@@ -1,5 +1,5 @@
 <?php
 // Using mysqli (connecting from App Engine)
-$link = mysql_connect('173.194.225.117', 'root', '') or die('No se pudo conectar: ' . mysql_error());
+$link = mysql_connect('173.194.225.117', 'root', 'toor') or die('No se pudo conectar: ' . mysql_error());
 mysql_select_db('iec');
 ?>
